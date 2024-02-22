f2b81327492fdbfa6cec819c0dbf1d866578980a
KushSawlani <sawlanikush@gmail.com>
2024-02-22 18:18:57 +0530

    Updated Ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import './App.css';
-import ImageCarousel from './ImageCarousel';
+
 
 function App() {
   const { loginWithRedirect,logout,user, isAuthenticated, isLoading } = useAuth0();
